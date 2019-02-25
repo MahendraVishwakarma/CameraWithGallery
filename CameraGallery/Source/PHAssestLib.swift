6d81090870b83636067a10931cd5a122df4df649
Mahendra Vishwakarma <mahendra@Mahendras-MacBook-Air.local>
2019-02-26 02:38:07 +0530

    Initial Commint by me

---
+++ CameraGallery/Source/PHAssestLib.swift
@@ -0,0 +1,47 @@
+//
+//  PHAssestLib.swift
+//  CameraGallery
+//
+//  Created by Mahendra Vishwakarma on 26/02/19.
+//  Copyright © 2019 Mahendra Vishwakarma. All rights reserved.
+//
+
+import Foundation
+import Photos
+import UIKit
+
+class PHAssestLib{
+    
+   static var photos = [UIImage]()
+    class func fetchImage(completion: @escaping ((Array<UIImage>) -> ())) {
+        let images = PHAsset.fetchAssets(with: .image, options: nil) // PHAsset.fetchAssets(with: fetchOptions)
+        
+        let options = PHImageRequestOptions()
+        options.version = .original
+        
+        for countIndex in 0..<images.count {
+            let assest = images.object(at: countIndex)
+            
+            PHImageManager.default().requestImageData(for: assest, options: nil) {
+                data, uti, orientation, info in
+                
+                guard let data =  data else{
+                    return
+                }
+                
+                guard let image = UIImage(data: data) else{
+                    return
+                }
+                
+                self.photos.append(image)
+                if(self.photos.count == images.count){
+                    completion(self.photos)
+                }
+                
+            }
+            
+        }
+    }
+}
+
+
---
+++ CameraGallery/ViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  CameraGallery
+//
+//  Created by Mahendra Vishwakarma on 25/02/19.
+//  Copyright © 2019 Mahendra Vishwakarma. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class imageCell:UICollectionViewCell{
+    @IBOutlet weak var image: UIImageView!
+    
+}
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    var photos = [UIImage]()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCaptureSession()
+        
+
+    }
+    
+    var captureSession = AVCaptureSession()
+    var backCamera : AVCaptureDevice?
+    var frontCamera : AVCaptureDevice?
+    var currentCamera : AVCaptureDevice?
+    var photoOutput : AVCapturePhotoOutput?
+    var cameraPreviewLayer : AVCaptureVideoPreviewLayer?
+    var image:UIImage?
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        PHAssestLib.fetchImage { (images) in
+            self.photos.append(contentsOf: images)
+            self.collectionView.reloadData()
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! imageCell
+        cell.image.image = photos[indexPath.row]
+        return cell
+    }
+    
+    func setupCaptureSession(){
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+    }
+
+    func setupDevice(){
+        
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices{
+            if device.position == AVCaptureDevice.Position.back{
+                backCamera = device
+            }else if device.position == AVCaptureDevice.Position.front{
+                frontCamera = device
+            }
+        }
+        currentCamera = backCamera
+    }
+    
+    func setupInputOutput(){
+        
+        do{
+            if(currentCamera != nil){
+                let captureDeviceInput =  try AVCaptureDeviceInput(device: currentCamera!)
+                captureSession.addInput(captureDeviceInput)
+                photoOutput = AVCapturePhotoOutput()
+                photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey:AVVideoCodecType.jpeg])], completionHandler: nil)
+                captureSession.addOutput(photoOutput!)
+            }
+            
+        }catch let error{
+            print(error)
+        }
+    }
+    
+    func setupPreviewLayer(){
+        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = .portrait
+        cameraPreviewLayer?.frame = self.view.bounds
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+        
+        
+    }
+    
+    func startRunningCaptureSession(){
+        captureSession.startRunning()
+    }
+    
+    @IBAction func captureImage(_ sender: Any) {
+        let settings = AVCapturePhotoSettings()
+        photoOutput?.capturePhoto(with: settings, delegate: self)
+        
+        
+    }
+}
+
+extension ViewController:AVCapturePhotoCaptureDelegate{
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation(){
+            image = UIImage(data: imageData)
+            
+        }
+    }
+}
